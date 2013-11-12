0fc86192e1366335e207a315bfb6037a47123dce
Poisonedyoyo <poisonedyoyo@gmail.com>
2013-11-12 17:45:07 -0500

    Raw Quiz
    Very Minimalistic.

---
+++ ex28.py
@@ -0,0 +1,102 @@
+print "" # Exercise 28: Boolean Practice
+
+if True and True == True:
+    print "Correct"
+else:
+    print "Goodness gracious... Go review the flashcards. NOW!\t-3\n"
+
+if False and False == False:
+    print "Correct"
+else:
+    print "I thought I told you to memorize those tables!\t-8\n"
+
+if 1 == 1 and 2 == 1 == False:
+    print "Correct"
+else:
+    print "Why on earth would you try this exercise if you hadn't memorized your homework!?\t-13\n"
+
+if "test" == "test" == True:
+    print "Correct"
+else:
+    print "Why would you even attempt this if you weren't prepared?\t-18\n"
+
+if 1 == 1 or 2 != 1 == True:
+    print "Correct"
+else:
+    print "I just really don't get why you thought THAT was the answer!\t-23\n"
+
+if True and 1 == 1 == True:
+    print "Correct"
+else:
+    print "I don't understand why you're running this program when you should be STUDYING YOUR TABLES!\t-28\n"
+
+if False and 0 != 0 == False:
+    print "Correct"
+else:
+    print "Come on...this is one of the easiest problems I've ever seen.\t-33\n"
+
+if True or 1 == 1 == True:
+    print "Correct"
+else:
+    print "I'm not angry or even upset. I'm just disappointed.\t-38\n"
+
+if "test" == "testing" == False:
+    print "Correct"
+else:
+    print "Hah! And here I thought you were a good student!\t-43\n"
+
+if 1 != 0 and 2 == 1 == False:
+    print "Correct"
+else:
+    print "Are you even trying?\t-48\n"
+
+if "test" != "testing" == True:
+    print "Correct"
+else:
+    print """What did the rabbit say to the goose? "It's obvious you aren't understanding this.
+Maybe you should go back and study harder." Actually that's what I'm saying. To you.\t-53\n"""
+
+if "test" == 1 == False:
+    print "Correct"
+else:
+    print "How did you not get this one?\t-59\n"
+
+if not (True and False) == False:
+    print "Correct"
+else:
+    print "Even my four year old Niece got this one!\t-64\n"
+
+if not (1 == 1 and 0 != 1) == False:
+    print "Correct"
+else:
+    print "Geez...you sure are dense.\t-69\n"
+
+if not (10 == 1 or 1000 == 1000) == False:
+    print "Correct"
+else:
+    print "Just wait til the guys find out you failed this one!\t-74\n"
+
+if not (1 != 10 or 3 == 4) == False:
+    print "Correct"
+else:
+    print "I honestly don't think you're trying any more.\t-79\n"
+
+if not ("testing" == "testing" and "Zed" == "Cool Guy") == False:
+    print "Correct"
+else:
+    print "Look man, I'm not saying you should try harder, but...no actually I am. TRY HARDER!\t-84\n"
+
+if 1 == 1 and not ("testing" == 1 or 1 == 0) == True:
+    print "Correct"
+else:
+    print "Maybe you should try a new occupation?\t-89\n"
+
+if "chunky" == "bacon" and not (3 == 4 or 3 == 3) == False:
+   print "Correct"
+else:
+    print "Try again, nucklehead.\t-94\n"
+
+if 3 == 3 and not ("testing" == "testing" or "Python" == "Fun") == False:
+    print "Correct"
+else:
+    print "you suck at this.\t-99\n"
